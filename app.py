5ed306a65f6b180adb04ba23a8daf1037be79c80
Mikel Eguia Bengoa <166691140+MikelEguia@users.noreply.github.com>
2026-04-26 18:01:19 +0200

    Add database connection function

--- app.py
+++ app.py
@@ -8,6 +8,13 @@ load_dotenv("mysql.env")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 app = Flask(__name__)
+def get_db_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database="invitados"
+    )
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -31,3 +38,4 @@ def agregar_cita():
             return render_template('resultado2.html',mensaje=mensaje)
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+
